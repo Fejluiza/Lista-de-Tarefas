e399b639034a8c9cb695e610d91285c2ca3e7f4c
Fejluiza <fejluiza@gmail.com>
2024-05-19 19:30:55 -0300

    Lista de Tarefas com React

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Tarefas from './components/tarefas'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Tarefas />
+  </React.StrictMode>
+)
